6f7afd193c223c03cb75afc57362c44b378b68c3
Osinachi Nwagboso <Osinachi.Nwagboso@redspeed-int.com>
2023-03-18 16:17:51 +0000

    Add project files.

---
+++ BookStore/Controllers/BooksController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+	[Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookStoreContext _context;
+
+        public BooksController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBook()
+        {
+          if (_context.Book == null)
+          {
+              return NotFound();
+          }
+            return await _context.Book.ToListAsync();
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(string id)
+        {
+          if (_context.Book == null)
+          {
+              return NotFound();
+          }
+            var book = await _context.Book.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        // PUT: api/Books/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(string id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Books
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+          if (_context.Book == null)
+          {
+              return Problem("Entity set 'BookStoreContext.Book'  is null.");
+          }
+            _context.Book.Add(book);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (BookExists(book.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+        }
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(string id)
+        {
+            if (_context.Book == null)
+            {
+                return NotFound();
+            }
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Book.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookExists(string id)
+        {
+            return (_context.Book?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿namespace BookStore.Models
+{
+	public class Book
+	{
+		public string? Id { get; set; }
+
+		public string? Title { get; set; }
+		public string? Author { get; set; }
+		public string? Publisher { get; set; }
+		public string? ISBN { get; set; }
+		public string? Genre { get; set; }
+		public decimal Price { get; set; }
+
+		public string? Description { get; set; }
+
+		public string? ImageUrl { get; set; }
+
+		public string? Language { get; set; }
+
+		public int Pages { get; set; }
+
+		public string? Format { get; set; }
+
+		public string? Edition { get; set; }
+
+		public DateTime PublishDate { get; set; }
+
+		public string? Category { get; set; }
+
+		
+	}
+}
